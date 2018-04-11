6a3158887f0f821b214c7833f5ee2c43dd48827e
daedeloth <thijs@catlab.be>
2018-04-11 17:32:27 +0200

    Add .txt format, not because it's useful, but because the previous formatter was broken and now we have something to test.

--- src/Middleware/ResourceToOutput.php
+++ src/Middleware/ResourceToOutput.php
@@ -2,11 +2,16 @@
 
 namespace CatLab\Charon\Laravel\Middleware;
 
-use CatLab\Charon\Laravel\Models\ResourceResponse;
 use Closure;
+use CatLab\Charon\Models\ResourceResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ResourceToOutput
 {
+    const RETURN_TYPE_TEXT = 'text';
+    const RETURN_TYPE_JSON = 'json';
+
     /**
      * Handle an incoming request.
      *
@@ -19,9 +24,61 @@ class ResourceToOutput
         $response = $next($request);
 
         if ($response instanceof ResourceResponse) {
-            $response = \Response::json($response->getResource()->toArray());
+
+            $format = $this->getReturnFormat($request);
+            switch($format) {
+                case self::RETURN_TYPE_TEXT:
+                    return $this->toText($response);
+
+                case self::RETURN_TYPE_JSON:
+                default:
+                    return $this->toJSON($response);
+            }
+
         }
 
         return $response;
+    }
+
+    /**
+     * @param ResourceResponse $response
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function toJSON(ResourceResponse $response)
+    {
+        return \Response::json($response->getResource()->toArray());
+    }
+
+    /**
+     * @param ResourceResponse $response
+     * @return Response
+     */
+    protected function toText(ResourceResponse $response)
+    {
+        return new Response(
+            print_r($response->getResource()->toArray(), true),
+            200,
+            [
+                'Content-type' => 'text/text'
+            ]
+        );
+    }
+
+    /**
+     * @param Request $request
+     * @return string
+     */
+    protected function getReturnFormat(Request $request)
+    {
+        $format = $request->route('format');
+        switch (strtolower($format)) {
+            case 'txt':
+            case 'text':
+                return self::RETURN_TYPE_TEXT;
+
+            case 'json':
+            default:
+                return self::RETURN_TYPE_JSON;
+        }
     }
 }
