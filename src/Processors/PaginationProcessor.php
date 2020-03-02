fc86b4b11210c8eed9ef2b1b21fe3f74987dee7f
Thijs Van der Schaeghe <thijs@catlab.be>
2020-03-02 17:09:29 +0100

    Fix the nested where parameters in the pagination processor (so that cursor pagination works again).

--- src/Processors/PaginationProcessor.php
+++ src/Processors/PaginationProcessor.php
@@ -3,6 +3,10 @@
 namespace CatLab\Charon\Laravel\Processors;
 
 use CatLab\Base\Interfaces\Database\SelectQueryParameters;
+use CatLab\Base\Interfaces\Database\WhereParameter;
+use CatLab\Base\Interfaces\Grammar\AndConjunction;
+use CatLab\Base\Interfaces\Grammar\OrConjunction;
+use CatLab\Charon\Exceptions\NotImplementedException;
 use CatLab\Charon\Interfaces\Context;
 use CatLab\Charon\Interfaces\Processor;
 use CatLab\Charon\Interfaces\ResourceCollection;
@@ -11,6 +15,7 @@ use CatLab\Charon\Interfaces\ResourceDefinitionFactory;
 use CatLab\Charon\Interfaces\ResourceTransformer;
 use CatLab\Charon\Interfaces\RESTResource;
 use CatLab\Charon\Models\FilterResults;
+use CatLab\Charon\Models\Properties\Base\Field;
 use CatLab\Charon\Models\Values\Base\RelationshipValue;
 use CatLab\Laravel\Database\SelectQueryTransformer;
 use Illuminate\Database\Eloquent\Builder;
@@ -85,6 +90,10 @@ class PaginationProcessor extends \CatLab\Charon\Processors\PaginationProcessor
                 'next' => $cursor->getNext() ? $url . '?' . http_build_query($cursor->getNext()) : null,
                 'previous' => $cursor->getPrevious() ? $url . '?' . http_build_query($cursor->getPrevious()) : null
             ]);
+
+            $collection->addMeta('pagination', [
+                'cursors' => $cursor->toArray()
+            ]);
         }
 
         if ($filterResults) {
@@ -94,6 +103,61 @@ class PaginationProcessor extends \CatLab\Charon\Processors\PaginationProcessor
                 'current-page' => $filterResults->getCurrentPage(),
                 'last-page' => ceil($filterResults->getTotalRecords() / $filterResults->getRecords())
             ]);
+        }
+    }
+
+    /**
+     * @param ResourceTransformer $transformer
+     * @param Context $context
+     * @param ResourceDefinition $resourceDefinition
+     * @param SelectQueryParameters $filter
+     * @param \Illuminate\Database\Query\Builder $queryBuilder
+     * @throws NotImplementedException
+     */
+    protected function processProcessorFilters(
+        ResourceTransformer $transformer,
+        Context $context,
+        ResourceDefinition $resourceDefinition,
+        SelectQueryParameters $filter,
+        $queryBuilder = null
+    ) {
+        $this->processWhereFilters(
+            $transformer,
+            $context,
+            $resourceDefinition,
+            $filter->getWhere(),
+            $queryBuilder
+        );
+
+        // now we need to translate these to our own system
+        foreach ($filter->getSort() as $sort) {
+            $field = $sort->getEntity();
+            if ($field instanceof Field) {
+                $transformer->getQueryAdapter()->applyPropertySorting(
+                    $transformer,
+                    $field->getResourceDefinition(),
+                    $context,
+                    $field,
+                    $queryBuilder,
+                    $sort->getDirection()
+                );
+            } else {
+                throw new \InvalidArgumentException(
+                    'WhereParameter requires a Field to be set as entity; ' . get_class($field) . ' provided.'
+                );
+            }
+        }
+
+        $limit = $filter->getLimit();
+        if ($limit) {
+            $transformer->getQueryAdapter()->applyLimit(
+                $transformer,
+                $resourceDefinition,
+                $context,
+                $queryBuilder,
+                $limit->getAmount(),
+                $limit->getOffset()
+            );
         }
     }
 
@@ -109,5 +173,57 @@ class PaginationProcessor extends \CatLab\Charon\Processors\PaginationProcessor
         $parentEntity = null
     ) {
         // TODO: Implement processResource() method.
+    }
+
+    /**
+     * @param ResourceTransformer $transformer
+     * @param Context $context
+     * @param ResourceDefinition $resourceDefinition
+     * @param WhereParameter[] $whereParameters
+     * @param \Illuminate\Database\Query\Builder $queryBuilder
+     */
+    private function processWhereFilters(
+        ResourceTransformer $transformer,
+        Context $context,
+        ResourceDefinition $resourceDefinition,
+        $whereParameters,
+        $queryBuilder
+    ) {
+        foreach ($whereParameters as $whereParameter) {
+            if ($comparison = $whereParameter->getComparison()) {
+
+                $field = $comparison->getEntity();
+                if ($field instanceof Field) {
+                    $transformer->getQueryAdapter()->applyPropertyFilter(
+                        $transformer,
+                        $field->getResourceDefinition(),
+                        $context,
+                        $field,
+                        $queryBuilder,
+                        $comparison->getValue(),
+                        $comparison->getOperator()
+                    );
+                } else {
+                    throw new \InvalidArgumentException(
+                        'WhereParameter requires a Field to be set as entity; ' . get_class($field) . ' provided.'
+                    );
+                }
+            }
+
+            // Won't somebody please think of the children?!
+            foreach ($whereParameter->getChildren() as $child) {
+                if ($child instanceof AndConjunction) {
+                    $queryBuilder->where(function ($query) use ($transformer, $context, $resourceDefinition, $child) {
+                        $this->processWhereFilters($transformer, $context, $resourceDefinition, [$child->getSubject()], $query);
+                    });
+                } elseif ($child instanceof OrConjunction) {
+                    $queryBuilder->orWhere(function ($query) use ($transformer, $context, $resourceDefinition, $child) {
+                        $this->processWhereFilters($transformer, $context, $resourceDefinition, [$child->getSubject()], $query);
+                    });
+                } else {
+                    throw new \InvalidArgumentException("Got an unknown conjunction");
+                }
+            }
+        }
     }
 }
