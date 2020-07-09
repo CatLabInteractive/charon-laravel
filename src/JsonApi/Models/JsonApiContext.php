e5fb508ed11888de50d687f83edbd3bd82756085
Thijs Van der Schaeghe <thijs@catlab.be>
2020-07-09 17:48:40 +0200

    JSONAPI Context: when 'expanding' a property, also automatically include it in the field selection.

--- src/JsonApi/Models/JsonApiContext.php
+++ src/JsonApi/Models/JsonApiContext.php
@@ -11,11 +11,18 @@ use CatLab\Charon\Models\CurrentPath;
  */
 class JsonApiContext extends Context
 {
+    private $globalIncludeFields = [];
+
     /**
      * @var array
      */
     private $includeFields = [];
 
+    /**
+     * @param $resourceType
+     * @param array $fields
+     * @return $this
+     */
     public function includeFields($resourceType, array $fields)
     {
         $this->includeFields[$resourceType] = $fields;
@@ -28,12 +35,18 @@ class JsonApiContext extends Context
      */
     public function shouldShowField(CurrentPath $fieldPath)
     {
+        $field = $fieldPath->getTopField();
+
+        // check if this field was included
+        if (in_array($field->getDisplayName(), $this->globalIncludeFields)) {
+            return true;
+        }
+
         // not set? Return null to cause 'default behaviour'
         if (count($this->includeFields) === 0) {
             return null;
         }
 
-        $field = $fieldPath->getTopField();
         $resourceType = $field->getResourceDefinition()->getType();
 
         // not defined? Don't include!
@@ -42,5 +55,19 @@ class JsonApiContext extends Context
         }
 
         return in_array($field->getDisplayName(), $this->includeFields[$resourceType]);
+    }
+
+    /**
+     * @param string $field
+     * @return $this
+     */
+    public function expandField($field)
+    {
+        parent::expandField($field);
+
+        // also include globally
+        $this->globalIncludeFields[] = $field;
+
+        return $this;
     }
 }
